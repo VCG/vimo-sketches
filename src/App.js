e9231356d4152f05b1126ef038016c668072549e
jinhan <choi.jinhan@gmail.com>
2023-03-03 11:33:32 -0500

    roll back App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import BasicExample from "./examples/BasicExample";
-import NodeEdgeAttributesExample from "./examples/NodeEdgeAttributesExample";
-import SearchingMotifsInNeuprintExample from "./examples/SearchingMotifsInNeuprintExample";
+import React, { useEffect, useState } from "react";
+import Sketch from "./lib/Sketch/Sketch";
+import NeuprintExecutor from "./lib/Executors/NeuprintExecutor";
+import "./App.css";
 
 function App() {
-  // 1. Basic Example
-  return <BasicExample />;
+  // Wrap things in context that can use global context
+  const token = "ADD YOUR TOKEN HERE";
+  const data_server = "https://neuprint.janelia.org/";
+  const data_version = "hemibrain:v1.2.1";
+  const vimo_server = "http://localhost:4242"; //"https://vimo-server-bmcp5imp6q-uk.a.run.app";
+  const ne = new NeuprintExecutor(
+    data_server,
+    data_version,
+    token,
+    vimo_server
+  );
 
-  // 2. Node & Edge Attributes Example
-  // return <NodeEdgeAttributesExample />;
+  const processRequest = async (motifJson, lim) => {
+    const query = await ne.json2cypher(motifJson, lim);
+    console.log(query);
+    return query;
+  };
 
-  // 3. Searching Motifs in Neuprint
-  // return <SearchingMotifsInNeuprintExample />;
+  // Example for neuprint
+  const [isQuerying, setIsQuerying] = useState(false);
+  const [attributes, setAttributes] = useState({
+    getMotifCount: ne.getMotifCount,
+    getRelativeMotifCount: ne.getRelativeMotifCount,
+    isQuerying: isQuerying,
+  });
+  useEffect(async () => {
+    setAttributes({
+      ...attributes,
+      NodeFields: await ne.getNodeFields(),
+      EdgeFields: await ne.getEdgeFields(),
+    });
+  }, []);
+
+  return (
+    <div>
+      <Sketch processRequest={processRequest} attributes={attributes} />
+    </div>
+  );
+  // return <BasicExample />;
 }
 
 export default App;
