3f7c64468fa246fa6439c15265a9f5c24b05d3be
Jakob Troidl <jakob.troidl@googlemail.com>
2023-03-09 00:02:18 -0500

    remove THREE dependency

--- src/lib/Sketch/utils/rendering.js
+++ src/lib/Sketch/utils/rendering.js
@@ -1,16 +1,6 @@
-import * as THREE from "three";
 import { createTheme } from "@mui/material";
 
 export class Color {
-  static orange = new THREE.Color("rgb(255,154,0)");
-
-  static white = new THREE.Color("rgb(255,255,255)");
-  static red = new THREE.Color("rgb(227,55,55)");
-  static grey = new THREE.Color("rgb(229,229,229)");
-  static pink = new THREE.Color("rgb(200,0,255)");
-  static blue = new THREE.Color("rgb(62,62,231)");
-  static black = new THREE.Color("rgb(0,0,0)");
-
   static theme = createTheme({
     palette: {
       stronger: {
